13df41a1a2e6e54eb8cfcef91bea1ee747edc5f1
Kyle Rader <kylewrader@gmail.com>
2022-08-04 19:20:01 -0700

    Doc the commands and args

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,24 @@ use clap::Parser;
 
 #[derive(Debug, Parser)]
 struct Target {
+    /// Scopes to request
     #[clap(long)]
     scopes: Option<Vec<String>>,
+    /// Client ID
     #[clap(long)]
     client: Option<String>,
+    /// Tenant ID
     #[clap(long)]
     tenant: Option<String>,
 }
 
+/// Do Auth Well
 #[derive(Debug, Parser)]
+#[clap(version)]
 enum Args {
+    /// Do the auth well
     Auth(Target),
+    /// No more token
     Clear(Target),
 }
 
