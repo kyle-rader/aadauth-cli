ea9e94dd8f7384b2d3353eff1dcdbfc134797d5c
Kyle Rader <kylewrader@gmail.com>
2022-08-04 19:14:43 -0700

    Added a Target struct for re-usable options for both commands

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
 use clap::Parser;
 
 #[derive(Debug, Parser)]
+struct Target {
+    #[clap(long)]
+    scopes: Option<Vec<String>>,
+    #[clap(long)]
+    client: Option<String>,
+    #[clap(long)]
+    tenant: Option<String>,
+}
+
+#[derive(Debug, Parser)]
 enum Args {
-    Auth,
-    Clear,
+    Auth(Target),
+    Clear(Target),
 }
 
 fn main() {
     match Args::parse() {
-        Args::Auth => println!("We're going to auth"),
-        Args::Clear => println!("We're going to clear"),
+        Args::Auth(_) => println!("We're going to auth"),
+        Args::Clear(_) => println!("We're going to clear"),
     }
 }
